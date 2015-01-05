e33b14f616fcbbaf802c34b81aed199f38073acb
sunzhuoshi <sunzhuoshi@gmail.com>
2015-01-05 15:26:28 +0800

    Initial commit

---
+++ Gifer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gifer
+//
+//  Created by sun zhuoshi on 1/5/15.
+//  Copyright (c) 2015 sunzhuoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Gifer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Gifer
+//
+//  Created by sun zhuoshi on 1/5/15.
+//  Copyright (c) 2015 sunzhuoshi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
